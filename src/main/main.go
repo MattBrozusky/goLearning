2e10f21e1ac020f6efcde15c7787988eb78b9dc1
MattBrozusky <matthew.brozusky@gmail.com>
2019-02-19 12:30:50 -0600

    Finished intial shuffle method

--- src/main/main.go
+++ src/main/main.go
@@ -1,10 +1,8 @@
 package main
 
 func main() {
-	// cards := newDeck()
-
-	// cards.saveToFile("my_cards")
-
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
+
 }
