195faadcab09273a8cb1bc7935dc62b676d9544b
MattBrozusky <matthew.brozusky@gmail.com>
2019-02-20 14:17:14 -0600

    finished interface walkthrough

--- src/interfacePractice/main.go
+++ src/interfacePractice/main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 
-	resp, err := http.Get("http://google.com")
+	resp, err := http.Get("http://patientplus.site")
 
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(resp.Body)
+	lw := logWriter{}
 
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just worte this many bytes:", len(bs))
+	return len(bs), nil
 }
